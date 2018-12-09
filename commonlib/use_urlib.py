fd2428e4acc7c3107b72f7b32f1f3b5f9c24efc9
fxyfdf <fxyfdf@sina.com>
2018-12-09 15:44:27 +0800

    Default Changelist

--- commonlib/use_urlib.py
+++ commonlib/use_urlib.py
@@ -20,5 +20,6 @@ req.add_header('User-Agent', 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS
                              'Version/8.0 Mobile/10A5376e Safari/8536.25')
 with request.urlopen(req) as f:
     print ('Status:',f.status,f.reason)
-    for k,v in f.getheaders()
+    for k,v in f.getheaders():
+        print (k,v)
 
